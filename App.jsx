fd2d9f1c229b6a3fe962674a8f19b5569dc1d6c2
maxi790 <maxicip@hotmail.com>
2024-10-23 17:53:25 -0700

    prueba

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import Shop from './pages/shop';
-import Contact from './pages/contact';
-import Home from './pages/home';
+import Navbar from './vite-project/src/components/navbar';
+import Footer from './vite-project/src/components/footer';
+import Shop from './vite-project/src/pages/shop';
+import Contact from './vite-project/src/pages/contact';
+import Home from './vite-project/src/pages/home';
 import './App.css';
 
 function App() {
